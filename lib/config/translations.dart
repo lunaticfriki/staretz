dec172cdd054c6001f072873397eab7d7c1c6360
lunaticfriki <ovenor11@proton.me>
2026-03-04 11:36:43 +0100

    translations

--- lib/config/translations.dart
+++ lib/config/translations.dart
@@ -12,11 +12,10 @@ class AppTranslations {
   static const String unknownState = 'Estat desconegut';
   static const String comingSoon = 'Encara no hi ha res, ja vindran, ja...';
 
-  // Menu Tags
   static const String menuHome = 'Inici';
   static const String menuMusic = 'Música';
   static const String menuVideogames = 'Videojocs';
   static const String menuPhilosophy = 'Filosofia';
   static const String menuProgramming = 'Programació';
-  static const String menuAbout = 'Sobre mi';
+  static const String menuAbout = 'Info';
 }
